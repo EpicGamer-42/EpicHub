f3c86f879c59377332a8a5affe8abd1478abf7e3
EpicGamer-42 <111116205+EpicGamer-42@users.noreply.github.com>
2022-08-17 01:39:31 -0500

    Create EpicHub.lua

---
+++ EpicHub.lua
@@ -0,0 +1,6 @@
+if game.PlaceId == 9598746251 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/EpicGamer-42/EpicHub/main/HomeRunSimulator.lua"))()
+  --elseif game.PlaceId == 9831850570 then
+      --loadstring(game:HttpGet("FOOTBALL PRO SIMULATOR SCRIPT COMING SOON"))()
+        else return
+end
